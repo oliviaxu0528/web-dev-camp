a993f99d2948d8b0fd2f3feb7263ef1d71d9b168
Olivia Xu <oliviaxu0528@Gmail.com>
2023-05-08 15:06:11 -0700

    drum done

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,12 +6,70 @@
 
 var numberOfDrumButtons = document.querySelectorAll("button").length;
 
+
+// detecting button press
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-        alert("I got clicked!");
-
+        var buttonInnerHTML = this.innerHTML;
+        // console.log(buttonInnerHTML);
+        makesound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
-
 }
 
+//detecting keyboard press
+document.addEventListener("keydown", (event) => {
+    // console.log(event.key);
+    makesound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makesound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "l":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+
+        default:
+    };
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
