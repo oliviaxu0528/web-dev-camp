92b636db98c289b6d7092458db606778a81ac604
Olivia Xu <oliviaxu0528@Gmail.com>
2023-05-22 16:25:53 -0700

    signup page done

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send("hello world!!")
+    res.sendFile(__dirname + "/signup.html")
 })
 app.listen(3000, () => {
     console.log(`example listen to 3000`)
